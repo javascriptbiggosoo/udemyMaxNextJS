606095ff430c097dd8cabd186e4181699bac3c8a
tbs01215 <tbs01215@gmail.com>
2024-10-03 18:24:03 +0900

    201003

--- foodies/app/meals/page.jsx
+++ foodies/app/meals/page.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/Meals/MealsGrid";
 
 export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          당신이 손수 만든 맛있는{" "}
-          <span className={classes.highlight}>음식들</span>
+          <span className={classes.highlight}>당신이 손수 만든 </span> 맛있는
+          음식들
         </h1>
         <p>마음에드는 레시피를 찾고 요리해보세요. 쉽고 즐겁습니다!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
